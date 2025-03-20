064a34b105204398aac568c1758edb59aafd56e0
torresvisuals-portfolio <92391645+torresvisuals-portfolio@users.noreply.github.com>
2025-03-20 19:29:04 +0100

    Fixed PWA icons and mobile chest grid layout

--- sw.js
+++ sw.js
@@ -1,24 +1,32 @@
 const CACHE_NAME = 'relic-tracker-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json',
+    './src/img/icon-72x72.png',
+    './src/img/icon-96x96.png',
+    './src/img/icon-128x128.png',
+    './src/img/icon-144x144.png',
+    './src/img/icon-152x152.png',
+    './src/img/icon-167x167.png',
+    './src/img/icon-180x180.png',
+    './src/img/icon-192x192.png',
+    './src/img/icon-384x384.png',
+    './src/img/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then((response) => response || fetch(event.request))
     );
 }); 
